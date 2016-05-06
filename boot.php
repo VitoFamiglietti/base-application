7cbd5a8b64ff91ecf08b6fc0f2260e4c365b2a67
VitoFamiglietti <vito.famiglietti@gmail.com>
2016-05-07 00:56:46 +0200

    moved bootstrap from index to boot.php, add classmap to autoload

--- boot.php
+++ boot.php
@@ -4,14 +4,38 @@
  * Application Bootstrap
  */
 
-// Define your custom logic to retrieve the current APP_ENV
-if (strrpos($_SERVER['HTTP_HOST'],'.local')===0){
-	define ('APP_ENV','development');
-} else if (strpos($_SERVER['HTTP_HOST'],'stage.')===0){
-	define ('APP_ENV','staging');
-} else {
-	define ('APP_ENV','production');
-}
+define ('APP_DIR', __DIR__);
+require APP_DIR.'/vendor/autoload.php';
 
-// Load environment-specific config
-Options::loadPHP(APP_DIR.'/configs/'.APP_ENV.'.php');
+$_response_send = new Deferred(function() {
+    Response::sent() || Response::send();
+});
+
+// Load options
+Options::loadPHP(APP_DIR.'/config.php');
+
+// Temp directory
+define ('TEMP_DIR', Options::get('cache.directory', sys_get_temp_dir()));
+
+// Caching strategy
+Cache::using([
+  'files' => [
+    'cache_dir' => TEMP_DIR
+  ],
+]);
+
+// Init Views
+View::using(new View\Twig(APP_DIR.'/templates',[
+    'cache'         => Options::get('cache.views',true) ? TEMP_DIR : false,
+    'auto_reload'   => Options::get('debug',false),
+]));
+
+View::addGlobals([
+  'BASE_URL'  => rtrim(dirname($_SERVER['PHP_SELF']),'/').'/',
+  'CACHEBUST' => Options::get('debug',false) ? '?v='.time() : '',
+]);
+
+// Routes
+foreach (glob(APP_DIR.'/routes/*.php') as $routedef) include $routedef;
+Event::trigger('app.dispatch');
+Route::dispatch();
--- config.php
+++ config.php
@@ -5,7 +5,6 @@
  */
 
 return [
-  'debug' => false, // Disable when in production
   'cache' => [
     'directory' => null, // if null will use the sys_get_temp_dir
     'views'     => true,
